f49dde119e6d31b68f68ca513de4f5721350f6cd
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-26 21:07:57 +0600

    Create Banner Component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="">
-      <button className="btn btn-primary">Test Button</button>
+      
     </div>
   );
 }
