5f8fee2993a22f81614f7464351faefe79d5f800
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-26 18:01:50 +0600

    Make Popping As Global Font Style

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,15 +1,12 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// initialize font
+const poppins = Poppins(
+  {
+    weight: ['100', '200', '400', '500', '600', '800'],
+  }
+);
 
 export const metadata = {
   title: "Create Next App",
@@ -20,7 +17,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        // use the font to entire html body
+        className={`${poppins.className} antialiased`}
       >
         {children}
       </body>
