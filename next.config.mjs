566d135805ffcae82d6a5da351219f1c46ceccf7
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-27 08:30:24 +0600

    Fix: Image Loading By Implementing Remote Image Optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  /* Remote Image Optimization */
+  images: {
+    remotePatterns: [new URL('https://i.ibb.co.com/**')],
+  },
 };
 
 export default nextConfig;
