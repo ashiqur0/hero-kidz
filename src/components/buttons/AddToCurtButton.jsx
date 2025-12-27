9cbcadbfdfd27d5239c6babd888e8e88b573447a
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-27 21:23:56 +0600

    Create AddToCurtButton Component Separately As Client Component As It Will Interact With Client

---
+++ src/components/buttons/AddToCurtButton.jsx
@@ -0,0 +1,13 @@
+'use client'
+
+const AddToCurtButton = () => {
+    return (
+        <div>
+            <button className="btn btn-primary mt-4 w-full md:w-auto">
+                Add To Cart
+            </button>
+        </div>
+    );
+};
+
+export default AddToCurtButton;
