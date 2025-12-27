3462d8a40d823c4cbb3b9ed4d929ca9be04ac8e0
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-27 14:15:48 +0600

    Update not-found Component, Added Link Button To Go Home

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,10 +1,12 @@
+import Link from "next/link";
 import { BiSolidErrorAlt } from "react-icons/bi";
 
 const Error404 = () => {
     return (
         <div className='flex flex-col min-h-screen justify-center items-center'>
-            <BiSolidErrorAlt size={140} />
-            <h1 className="md:text-6xl text-4xl font-semibold mt-10">Page not found</h1>
+            <BiSolidErrorAlt size={100}  className="text-primary"/>
+            <h1 className="text-4xl font-semibold mt-10">Page not found</h1>
+            <Link href={'/'} className="btn btn-soft btn-primary border border-red-500 mt-5">Go Home</Link>
         </div>
     );
 };
