577a37846964340cd265d4224f6bf93d4b25fadb
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-27 21:15:56 +0600

    Create Sign up Page

--- src/app/auth/register/page.jsx
+++ src/app/auth/register/page.jsx
@@ -1,19 +1,25 @@
 'use client'
 
-const Login = () => {
+const page = () => {
     return (
         <div className='min-h-screen flex justify-center items-center'>
             <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                 <div className="card-body">
-                    <h1 className='text-center text-2xl font-semibold mb-5'>Login</h1>
+                    <h1 className='text-center text-2xl font-semibold mb-5'>Sign up</h1>
                     <fieldset className="fieldset">
+
+                        <label className="label">Full Name</label>
+                        <input type="text" className="input" placeholder="Your name" />
+
                         <label className="label">Email</label>
                         <input type="email" className="input" placeholder="Email" />
+
                         <label className="label">Password</label>
                         <input type="password" className="input" placeholder="Password" />
+
                         <div><a className="link link-hover">Forgot password?</a></div>
 
-                        <button className="btn btn-soft btn-primary border border-red-500 mt-4">Login</button>
+                        <button className="btn btn-soft btn-primary border border-red-500 mt-4">Sign up</button>
                     </fieldset>
                 </div>
             </div>
@@ -21,4 +27,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default page;
