915e79d5dad8092c78ac9af00a334cce4e816478
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-27 09:21:07 +0600

    Update Products Component, Now Latest Products Are Loading From Database

--- src/components/home/Products.jsx
+++ src/components/home/Products.jsx
@@ -1,14 +1,17 @@
-import products from '@/app/data/toys.json';
 import ProductCard from '../cards/ProductCard';
+import { latestProducts } from '@/actions/server/products';
 
-const Products = () => {
+const Products = async () => {
+
+    const latest_products = (await latestProducts()) || [];
+
     return (
         <div className=''>
             <h1 className='text-center text-4xl font-bold mb-10'>Our Products</h1>
 
             <div className='grid md:grid-cols-3 gap-5'>
                 {
-                    products.map(product => <ProductCard key={product.title} product={product}>{product.title}</ProductCard>)
+                    latest_products.map(product => <ProductCard key={product.title} product={product}>{product.title}</ProductCard>)
                 }
             </div>
         </div>
