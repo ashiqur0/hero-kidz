905e329c5ca45c9a8442afb9b74ea4619b54f4a5
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-27 22:39:34 +0600

    Update Sign up Form, Create handleRegister Function To Manage Sign up

--- src/app/(auth)/signup/page.jsx
+++ src/app/(auth)/signup/page.jsx
@@ -3,27 +3,56 @@
 import Link from "next/link";
 
 const page = () => {
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+        console.log({ name, email, password });
+    }
+
     return (
         <div className='min-h-screen flex justify-center items-center'>
             <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                 <div className="card-body">
                     <h1 className='text-center text-2xl font-semibold mb-5'>Sign up</h1>
-                    <fieldset className="fieldset">
+                    <form onSubmit={handleRegister}>
+                        <fieldset className="fieldset">
 
-                        <label className="label">Full Name</label>
-                        <input type="text" className="input" placeholder="Your name" />
+                            <label className="label">Full Name</label>
+                            <input
+                                type="text"
+                                className="input"
+                                placeholder="Your name"
+                                name="name"
+                                required
+                            />
 
-                        <label className="label">Email</label>
-                        <input type="email" className="input" placeholder="Email" />
+                            <label className="label">Email</label>
+                            <input
+                                type="email"
+                                className="input"
+                                placeholder="Email"
+                                name='email'
+                                required
+                            />
 
-                        <label className="label">Password</label>
-                        <input type="password" className="input" placeholder="Password" />
+                            <label className="label">Password</label>
+                            <input
+                                type="password"
+                                className="input"
+                                placeholder="Password"
+                                name='password'
+                                required
+                            />
 
-                        <div><a className="link link-hover">Forgot password?</a></div>
-
-                        <button className="btn btn-soft btn-primary border border-red-500 mt-4">Sign up</button>
-                        <Link href={'/login'}>Already have an account? <span className='text-blue-500 mt-2'>login</span></Link>
-                    </fieldset>
+                            <button type="submit" className="btn btn-soft btn-primary border border-red-500 mt-4">Sign up</button>
+                            <Link href={'/login'}>Already have an account? <span className='text-blue-500 mt-2'>login</span></Link>
+                        </fieldset>
+                    </form>
                 </div>
             </div>
         </div>
