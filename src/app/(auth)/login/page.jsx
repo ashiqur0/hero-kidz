23dbd0718a453363c5f5d864a05f6fdd37b61d24
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-27 22:33:23 +0600

    Update Login Form, Create handleLogin Function To Manage Login

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -2,21 +2,46 @@
 import Link from 'next/link';
 
 const Login = () => {
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log({ email, password });
+    }
+
     return (
         <div className='min-h-screen flex justify-center items-center'>
             <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                 <div className="card-body">
                     <h1 className='text-center text-2xl font-semibold mb-5'>Login</h1>
-                    <fieldset className="fieldset">
-                        <label className="label">Email</label>
-                        <input type="email" className="input" placeholder="Email" />
-                        <label className="label">Password</label>
-                        <input type="password" className="input" placeholder="Password" />
-                        <div><a className="link link-hover">Forgot password?</a></div>
+                    <form onSubmit={handleLogin}>
+                        <fieldset className="fieldset">
 
-                        <button className="btn btn-soft btn-primary border border-red-500 mt-4">Login</button>
-                        <Link href={'/signup'}>Do not have an account? <span className='text-blue-500 mt-2'>sign up</span></Link>
-                    </fieldset>
+                            <label className="label">Email</label>
+                            <input
+                                type="email"
+                                className="input"
+                                placeholder="Email"
+                                name='email'
+                                required
+                            />
+
+                            <label className="label">Password</label>
+                            <input
+                                type="password"
+                                className="input"
+                                placeholder="Password"
+                                name='password'
+                                required
+                            />
+                            <div><a className="link link-hover">Forgot password?</a></div>
+
+                            <button type='submit' className="btn btn-soft btn-primary border border-red-500 mt-4">Login</button>
+                            <Link href={'/signup'}>Do not have an account? <span className='text-blue-500 mt-2'>sign up</span></Link>
+                        </fieldset>
+                    </form>
                 </div>
             </div>
         </div>
