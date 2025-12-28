7bb42db18729ec9b2e84420fd04c2bd64f64eeaf
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-28 10:55:07 +0600

    Create psotUser Method To Post User On Database

--- src/actions/server/auth.js
+++ src/actions/server/auth.js
@@ -1,8 +1,34 @@
 'use server'
 
+import { collections, dbConnect } from "@/lib/dbConnect";
+import bcrypt from 'bcryptjs';
+
 export const postUser = async (payload) => {
+    const { email, password, name } = payload;
+
     // 1. check payload
+    if (!email || !password) return null;
+
     // 2. check user
+    const isExists = await dbConnect(collections.USERS).findOne({ email });
+    if (isExists) return null;
+
     // 3. create user
+    const newUser = {
+        provider: 'credentials',
+        name,
+        email,
+        password: await bcrypt.hash(password, 14),
+        role: 'users',
+        createdAt: new Date().toISOString()
+    };
+
     // 4. insert user
+    const result = await dbConnect(collections.USERS).insertOne(newUser);
+    if (result.acknowledge) {
+        return {
+            ...result,
+            insertedId: result.insertedId.toString()
+        }
+    }
 } 
