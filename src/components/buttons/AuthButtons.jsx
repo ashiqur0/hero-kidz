279e6c6c7311f2075189f2bfcb9efbcd0b3ae280
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-12-28 12:53:45 +0600

    Update AuthButtons Component, Make Log out, and SignIn, SignUp Buttons Conditional Appearing By session status

--- src/components/buttons/AuthButtons.jsx
+++ src/components/buttons/AuthButtons.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+'use client'
+
+import { signOut, useSession } from 'next-auth/react';
+import Link from 'next/link';
 
 const AuthButtons = () => {
+
+    const session = useSession();
+
     return (
         <div>
-            
+            {
+                session.status == 'authenticated' ? <>
+                    <button onClick={() => signOut()} className='btn btn-primary'>Log out</button>
+                </> : <>
+                    <Link href={'/login'} className='btn btn-primary btn-outline'>Sign In</Link>
+                    <Link href={'/signup'} className='btn btn-primary btn-outline'>Sign up</Link>
+                </>
+            }
         </div>
     );
 };
