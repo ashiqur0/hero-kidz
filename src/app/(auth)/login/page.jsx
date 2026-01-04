72adeb511a54acb4d5e32e733f569b625d54371c
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-01-05 04:31:11 +0600

    Create LoginForm Component Sepepately As Client Component

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,62 +1,12 @@
-'use client'
-import Link from 'next/link';
-import { useRouter, useSearchParams } from 'next/navigation';
-import { signIn } from "next-auth/react"
-import Swal from 'sweetalert2';
 import SocialLoginButtons from '@/components/buttons/SocialLoginButtons';
 
 const Login = () => {
-
-    const callbackUrl = useSearchParams().get('callbackUrl');
-    const router = useRouter();
-
-    const handleLogin = async (e) => {
-        e.preventDefault();
-
-        const email = e.target.email.value;
-        const password = e.target.password.value;
-        const result = await signIn('credentials', { redirect: false, email, password });
-        if (result.ok) {
-            Swal.fire('success', 'Welcome to Hero Kidz', 'success');
-            router.push('/');
-        } else {
-            Swal.fire('error', 'Email and password not matched', 'error');
-        }
-    }
 
     return (
         <div className='min-h-screen flex justify-center items-center'>
             <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                 <div className="card-body">
                     <h1 className='text-center text-2xl font-semibold mb-5'>Login</h1>
-                    <form onSubmit={handleLogin}>
-                        <fieldset className="fieldset">
-
-                            <label className="label">Email</label>
-                            <input
-                                type="email"
-                                className="input"
-                                placeholder="Email"
-                                name='email'
-                                required
-                            />
-
-                            <label className="label">Password</label>
-                            <input
-                                type="password"
-                                className="input"
-                                placeholder="Password"
-                                name='password'
-                                required
-                            />
-                            <div><a className="link link-hover">Forgot password?</a></div>
-
-                            <button type='submit' className="btn btn-soft btn-primary border border-red-500 mt-4">Login</button>
-
-                            <Link href={`/signup?callbackUrl=${callbackUrl}`}>Do not have an account? <span className='text-blue-500 mt-2'>sign up</span></Link>
-                        </fieldset>
-                    </form>
-
                     <SocialLoginButtons />
                 </div>
             </div>
---
+++ src/components/forms/LoginForm.jsx
@@ -0,0 +1,58 @@
+'use client'
+
+import Link from 'next/link';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { signIn } from "next-auth/react"
+import Swal from 'sweetalert2';
+
+const LoginForm = () => {
+
+    const callbackUrl = useSearchParams().get('callbackUrl');
+    const router = useRouter();
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        const result = await signIn('credentials', { redirect: false, email, password });
+        if (result.ok) {
+            Swal.fire('success', 'Welcome to Hero Kidz', 'success');
+            router.push('/');
+        } else {
+            Swal.fire('error', 'Email and password not matched', 'error');
+        }
+    }
+
+    return (
+        <form onSubmit={handleLogin}>
+            <fieldset className="fieldset">
+
+                <label className="label">Email</label>
+                <input
+                    type="email"
+                    className="input"
+                    placeholder="Email"
+                    name='email'
+                    required
+                />
+
+                <label className="label">Password</label>
+                <input
+                    type="password"
+                    className="input"
+                    placeholder="Password"
+                    name='password'
+                    required
+                />
+                <div><a className="link link-hover">Forgot password?</a></div>
+
+                <button type='submit' className="btn btn-soft btn-primary border border-red-500 mt-4">Login</button>
+
+                <Link href={`/signup?callbackUrl=${callbackUrl}`}>Do not have an account? <span className='text-blue-500 mt-2'>sign up</span></Link>
+            </fieldset>
+        </form>
+    );
+};
+
+export default LoginForm;
